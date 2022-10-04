4f4bb1147a280d3bdfd00035a9068410e92d1ac0
Ali Basheer <66694679+alibasheras1234@users.noreply.github.com>
2022-10-04 14:59:51 +0300

    Fedora

--- first.py
+++ first.py
@@ -1,19 +1,3 @@
-# -------------------------------------------
-# -- Variables --
-# --------------
-# Source Code : Original Code You Write It In Computer
-# Translation : Converting Source Code Into Machine Language
-# Compilation : Translate Code Before Run Time
-# Run-Time    : Period App Take To Executing Commands
-# Interpreted : Code Translated On The FLy During Execution
-# -------------------------------------------
-# -- Information's --
-# -----------------
-# Python Is Dynamically Typed Language
-# -------------------------------------------
+import os
 
-# Reserved Words
-help("keywords")
-
-a, b, c = 1, 2, 3
-print(a, b, c)
+os.system("REX.png")
