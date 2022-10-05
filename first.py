5d883de92d570e694c26946c439b71097f52bf44
alibasher1200 <alibasher2000.16.11@gmail.com>
2022-10-06 01:11:26 +0300

    Concatenation

--- first.py
+++ first.py
@@ -1,44 +1,22 @@
 # ----------------------------------
-# -- Escaping --
-# --------------
-# Escape Sequences Characters
-# \b       => Back Space
-# \newline => Escape New Line + \
-# \        => Escape Back Slash
-# \'       => Escape Single Quote
-# \"       => Escape Double Quote
-# \n       => Line Feed Character
-# \r       => Carriage Return
-# \t       => Horizontal Tab
-# \xhh     => Character Hex Value
+# -------------------
+# -- Concatenation --
+# -------------------
+# Concatenation Just String With String
 # ----------------------------------
 
-# Back Space
-print("Hello In Java\b")  # Will Remove Character 'a'
+FName = 'Ali'
+LName = 'Basheer'
 
-# Escape New Line + \
-print("Hello \
-python \
-for \
-Everyone")
+print(FName + " " + LName)
 
-# Escape Back Slash
-print("I Love Back Slash\\")
+L1 = "line1\
+ Line2\
+ Line3"
 
-# Escape Single Quote
-print('I Love Single Quote \'Test\'')
+L2 = "line4\
+ Line4\
+ Line5"
 
-# Escape Double Quote
-print("I Love Single Quote \"Test\"")
-
-# Line Feed
-print("Hello World\nSecond Line")
-
-# Carriage Return
-print('123456\rAbcd')  # OutPut => Abcd56
-
-# Horizontal Tab
-print("Hello\t World")
-
-# Character Hex Value
-print("\x41\x4C\x49")
+print(L1 + " " + L2)
+print(L1 + "\n" + L2)
