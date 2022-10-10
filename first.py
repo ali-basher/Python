934c9911ed964ca46498aae2518a9d2baf316081
Ali Basheer <66694679+alibasheras1234@users.noreply.github.com>
2022-10-10 23:10:48 +0300

    String Indexing & Slicing

--- first.py
+++ first.py
@@ -1,5 +1,38 @@
-Name = 'Ali'
-Age = 22
-Country = 'Iraq'
+# -----------------------------
+# String Indexing & Slicing
+# [1] All Data in Python ismObject
+# [2] Object Contain Elements
+# [3] Every Element Has Its Own Index
+# [4] Python Use Zero Based Index ( Index Start From Zero )
+# [5] Use Square Brackets To Access Element
+# [6] Enable Accessing Parts Of Strings, Tuples or Lists
+# -----------------------------
 
-print('\"Hello, My  Is ' + Name + ' And Iam ' + str(Age) + ' Years Old and I Live in ' + Country + '.\"')
+# Indexing ( Access Single Item )
+
+MyString = "I Love Python"
+
+print(MyString[0])  # Index 0 => I
+print(MyString[9])  # Index 9 => t
+
+print(MyString[-1])  # Index -1 => First Character From End
+print(MyString[-6])  # Index -6 => 6th Character From End
+
+# Slicing ( Access Multiple Sequence Items )
+# [Start:End] End Not Included
+# [Start:End:Steps]
+
+print(MyString[2:6])  # Love
+print(MyString[7:11])  # Pyth
+
+print(MyString[:10])  # If Start Is Not Here Will Start From 0 ( I Love Pyt )
+print(MyString[5:])  # If Start Is Not Here Will Go The End~ ( e Python )
+
+print(MyString[:])  # Full Data
+
+print(MyString[0::1])  # Full Data
+print(MyString[::1])  # Full Data
+
+print(MyString[::2])
+print(MyString[::3])
+
